136fff6eae4be0de3f479e04875efb3210d736c9
Philip Fahim <philip.louis49@gmail.com>
2018-10-26 19:07:25 -0700

    feat: connected API and made components for styling purposes

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,7 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import Header from './Header.js';
+import AlbumList from './AlbumList.js';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -21,15 +22,19 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Header />
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View>
+        <Header headerText={'Albums'} />
+        <AlbumList />
       </View>
     );
   }
 }
+
+{/* <View style={styles.container}>
+  <Text style={styles.welcome}>Welcome to React Native!</Text>
+  <Text style={styles.instructions}>To get started, edit App.js</Text>
+  <Text style={styles.instructions}>{instructions}</Text>
+</View> */}
 
 const styles = StyleSheet.create({
   container: {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,32 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
-const Header = () => {
-    return <Text>Albums</Text>;
+const Header = (props) => {
+    const { textStyle, viewStyle } = styles;
+
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>{props.headerText}</Text>
+        </View>
+    );
+};
+
+const styles = {
+    viewStyle: {
+        backgroundColor: 'yellow',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 90,
+        paddingTop: 30,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.8,
+        elevation: 2,
+        position: 'relative'
+    },
+    textSyle: {
+        fontSize: 30
+    }
 };
 
 export default Header;
